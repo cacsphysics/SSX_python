e8e79b5eb40c1480eb26e5e0c0ebfdc6a7373c65
cacsphysics <ccartagena@brynmawr.edu>
2017-01-25 16:19:53 -0500

    Making X-Magnetic Plots
    These are the x-magnetic field plots for the each probe

--- instant_deviation_magn.py
+++ instant_deviation_magn.py
@@ -8,8 +8,8 @@ Created on Tue Jan 17 09:21:35 2017
 
 import pickle
 import numpy as np
-#import scipy.optimize as optimization
-#import math as m
+import scipy.optimize as optimization
+import math as m
 import matplotlib.pylab as plt
 
 #--------------------------------------
@@ -26,7 +26,7 @@ with open('mag123016_92_data.pickle') as f:
 #--------------------------------------
 def timePerspective(div):    
     d = 0.0
-    i = 1400
+    i = 1909
     while (d < div):
     
         for j in range(0,19):
@@ -42,15 +42,67 @@ def timePerspective(div):
 # final time step(fstep).
 #--------------------------------------
 def magneticTimeAvg(istep,fstep):
-    bAvg = np.zeros(19)
-    for i in range(0,19):
+    bAvg = np.zeros(20)
+    for i in range(0,20):
         for j in range(istep,fstep):
-            bAvg[i] = bAvg[i] + b[0,i,j]/10.0
+            bAvg[i] = bAvg[i] + b[0,i,j]/(fstep - istep + 1)
     return bAvg
 #--------------------------------------
-position = np.arange(19)*1.5
+
+
+#--------------------------------------
+# the plotting of a set of data
+#--------------------------------------
+def sine_plot(pos,bAvg)
+    plt.figure()
+    plt.clf()
+    plt.plot(position,bAvg,'o',linestyle = 'dotted' )
+    #plt.plot(position,bAvg,'o')
+    plt.xlabel('Position [cm]')
+    plt.ylabel('B [G]')
+#--------------------------------------
+
+#--------------------------------------
+# Defining the time averaged magnetic field
+# and position vectors
+#--------------------------------------
+bAvg = magneticTimeAvg(2500,2600)
+position = np.arange(20)*1.5
+#--------------------------------------
+
+"""
+#--------------------------------------
+# Plotting to obtain a visual of the data
+#--------------------------------------
 plt.figure(1)
 plt.clf()
 plt.plot(position,bAvg,'o',linestyle = 'dotted' )
 plt.xlabel('Position [cm]')
 plt.ylabel('B [G]')
+#--------------------------------------
+"""
+#--------------------------------------
+# Running the curve fit
+#--------------------------------------
+#--------------------------------------
+# Setting inital values
+#--------------------------------------
+"""magWeight = np.zeros(20)
+for i in range(1,21):
+    magWeight[-i] = (i+1)
+
+#magWeight = bAvg*0.1
+#magWeight = np.array([1,1,2,1,1,1,6,10,6,10,10,6,6,10,6,6,6,10,6,6])        
+
+xo = np.array([50.0, 2*m.pi/3.5, 0.0,0.0])
+bou = [(10.0, 0, -m.pi,-10.0), (150.0, 4*m.pi, m.pi, 10.0)]
+#--------------------------------------
+def func(x,a,b,c,d):
+    return a * np.sin(b*x + c) + d
+
+print optimization.curve_fit(func, position, bAvg, p0 = xo, 
+                             sigma = magWeight, bounds = bou)
+#--------------------------------------
+"""
+
+
---
+++ mjmag_plot_sav.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python2
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jan 23 15:13:40 2017
+This script creates and saves all figures
+@author: ccartagena
+"""
+
+import numpy as np
+import matplotlib.pylab as plt
+import pickle
+
+with open('mag123016_92_data.pickle') as f:
+    time,bdot,timeb,b,bmod = pickle.load(f)
+
+for j in range(0,20):   
+    plt.figure()
+    plt.title('X Probe ' + str(j + 1))
+    plt.plot(timeb,b[0,j,:])
+    plt.xlabel('Time [us]')
+    plt.ylabel('B [G]')
+    plt.savefig('X_Probe_' + str(j + 1))
+    plt.close()
