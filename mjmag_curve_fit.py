b29d7b10cfd889c19fb80c87ccfe798a3b07e131
cacsphysics <ccartagena@brynmawr.edu>
2017-01-25 16:20:51 -0500

    Editing the plotting routine

---
+++ mjmag_curve_fit.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python2
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jan 18 21:08:47 2017
+
+@author: ccartagena
+"""
+
---
+++ sine_data.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python2
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jan 10 23:26:23 2017
+
+@author: ccartagena
+"""
+
+import numpy as np
+import matplotlib.pylab as plt
+
+plt.figure(4)
+plt.clf()
+plt.plot(position,bAvg,'o',linestyle = 'dotted' )
+#plt.plot(position,bAvg,'o')
+plt.xlabel('Position [cm]')
+plt.ylabel('B [G]')
+
+distance = np.zeros(1050)
+
+for i in range(0,1050):
+    distance[i] = 0.0285 * i
+sine_data = 62.94 * np.sin((1.54*distance +  m.pi)) + 6.94
+
+#plt.plot(distance, sine_data)
