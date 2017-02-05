9852dba78f9f8bbc06531bd624006f9ebf580aec
cacsphysics <ccartagena@brynmawr.edu>
2017-02-05 00:04:45 -0500

    Moving and Creating
    Moving and preparing to load in other shots.
    Also, I made a few fits of shot 92.

--- instant_magn_analysis.py
+++ instant_magn_analysis.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 """
 Created on Tue Jan 17 09:21:35 2017
- This is the beginging of a library focusing on analysis
+ This is the beginning of a library focusing on analysis
 @author: ccartagena
 """
 
@@ -77,6 +77,7 @@ def sine_plot(pos, bAvg, title):
     plt.savefig(savefile, facecolor = 'w', edgecolor = 'k')
     plt.close()
 #--------------------------------------
+
 #--------------------------------------
 # the plotting of a set of x and y data
 #--------------------------------------
@@ -114,22 +115,8 @@ def sine_plot(pos, xbAvg, ybAvg, title):
     savefile = 'mjmag_' + title + '.png'
     fig.savefig(savefile)
     plt.close(fig)
-"""    plt.plot(pos, xbAvg, 'o', linestyle = 'dotted' )
-    #plt.plot(position,bAvg,'o')
-    #plt.xlabel('Position [cm]')
-    plt.ylabel('B [G]')
-    plt.ylim(-600, 600)
-    plt.title(title)
-    #savefile = 'mjmag_' + title + '.png'
-    #plt.savefig(savefile, facecolor = 'w', edgecolor = 'k')
-    
-    plt.subplot(2,1,2)
-    plt.plot(pos, ybAvg, 'o', linestyle = 'dotted')
-    plt.ylim(-600, 600)
-    plt.xlabel('Position [cm]')
-    plt.show()
-    #plt.close()
-"""
+#--------------------------------------
+
 #--------------------------------------
 # This function produces the figures for the  averages over
 # 100 steps. 
@@ -215,48 +202,3 @@ def plot_80avg_xpos_mag(istep, fstep):
         istep = istep + 80
 #--------------------------------------
 """
-"""
-#--------------------------------------
-# Defining the time averaged magnetic field
-# and position vectors
-#--------------------------------------
-bAvg = magneticTimeAvg(2500,2600)
-position = np.arange(20)*1.5
-#--------------------------------------
-
-
-#--------------------------------------
-# Plotting to obtain a visual of the data
-#--------------------------------------
-plt.figure(1)
-plt.clf()
-plt.plot(position,bAvg,'o',linestyle = 'dotted' )
-plt.xlabel('Position [cm]')
-plt.ylabel('B [G]')
-#--------------------------------------
-"""
-#--------------------------------------
-# Running the curve fit
-#--------------------------------------
-#--------------------------------------
-# Setting inital values
-#--------------------------------------
-"""magWeight = np.zeros(20)
-for i in range(1,21):
-    magWeight[-i] = (i+1)
-
-#magWeight = bAvg*0.1
-#magWeight = np.array([1,1,2,1,1,1,6,10,6,10,10,6,6,10,6,6,6,10,6,6])        
-
-xo = np.array([50.0, 2*m.pi/3.5, 0.0,0.0])
-bou = [(10.0, 0, -m.pi,-10.0), (150.0, 4*m.pi, m.pi, 10.0)]
-#--------------------------------------
-def func(x,a,b,c,d):
-    return a * np.sin(b*x + c) + d
-
-print optimization.curve_fit(func, position, bAvg, p0 = xo, 
-                             sigma = magWeight, bounds = bou)
-#--------------------------------------
-"""
-
-
